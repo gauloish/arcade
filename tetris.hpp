7d2442a8e8b1e1023b8df6c3064ea895e1c8ba81
Gaulöis <125079293+gauloish@users.noreply.github.com>
2023-03-31 14:59:00 -0300

    formatted tetris.hpp

--- tetris.hpp
+++ tetris.hpp
@@ -1,268 +1,233 @@
-#include "things.hpp"
 #include "geometry.hpp"
+#include "things.hpp"
 
 #ifndef TETRIS_HPP
 #define TETRIS_HPP
 
 namespace tetris {
-  canvas::frame frame;
+canvas::frame frame;
 
-  geometry::object<4> block;
+geometry::object<4> block;
 
-  short position{};
-  short rotation{};
-  short level{};
+short position{};
+short rotation{};
+short level{};
 
-  short steps{};
-  short count{};
-  short piece{};
-  short score{};
-  
-  bool change{};
-  bool press{};
+short steps{};
+short count{};
+short piece{};
+short score{};
 
-  short rotate[2]{};
+bool change{};
+bool press{};
 
-  void start();
-  void generate();
-  void read();
-  void draw();
-  bool collision();
-  void repos();
-  bool verify();
-  void update(int);
+short rotate[2]{};
 
-  canvas::frame image({
-    0b11111111,
-    0b10000001,
-    0b10100001,
-    0b10100001,
-    0b10101101,
-    0b10101101,
-    0b10000001,
-    0b11111111
-  });
+void start();
+void generate();
+void read();
+void draw();
+bool collision();
+void repos();
+bool verify();
+void update(int);
 
-  void start() {
+canvas::frame image({0b11111111, 0b10000001, 0b10100001, 0b10100001, 0b10101101, 0b10101101, 0b10000001, 0b11111111});
+
+void start() {
     numerics::seed();
 
     frame.clear();
 
     change = true;
-  }
+}
 
-  void generate() {
-    const static short lines[7][4] = {
-      {0, 0, 0, 0},
-      {0, 0, 0, 1},
-      {0, 0, 0, 1},
-      {0, 0, 0, 1},
-      {0, 0, 1, 1},
-      {1, 0, 0, 1},
-      {0, 0, 1, 1}
-    };
+void generate() {
+    const static short lines[7][4] = {{0, 0, 0, 0}, {0, 0, 0, 1}, {0, 0, 0, 1}, {0, 0, 0, 1},
+                                      {0, 0, 1, 1}, {1, 0, 0, 1}, {0, 0, 1, 1}};
 
-    const static short columns[7][4] = {
-      {-1, 0,  1,  2},
-      {-1, 0,  1, -1},
-      {-1, 0,  1,  0},
-      {-1, 0,  1,  1},
-      {-1, 0,  0,  1},
-      {-1, 0,  1,  0},
-      {-1, 0, -1,  0}
-    };
+    const static short columns[7][4] = {{-1, 0, 1, 2}, {-1, 0, 1, -1}, {-1, 0, 1, 0}, {-1, 0, 1, 1},
+                                        {-1, 0, 0, 1}, {-1, 0, 1, 0},  {-1, 0, -1, 0}};
 
-    block = geometry::object<4>({
-      geometry::point(lines[piece][0], columns[piece][0]),
-      geometry::point(lines[piece][1], columns[piece][1]),
-      geometry::point(lines[piece][2], columns[piece][2]),
-      geometry::point(lines[piece][3], columns[piece][3])
-    });
+    block = geometry::object<4>(
+        {geometry::point(lines[piece][0], columns[piece][0]), geometry::point(lines[piece][1], columns[piece][1]),
+         geometry::point(lines[piece][2], columns[piece][2]), geometry::point(lines[piece][3], columns[piece][3])});
 
     block = geometry::orthos(block, block[1], 4 - rotate[0]);
-  }
+}
 
-  void read() {
-    press = bool(
-      things::left.read()  ||
-      things::right.read() ||
-      things::top.read()   ||
-      things::bottom.read()
-    );
+void read() {
+    press = bool(things::left.read() || things::right.read() || things::top.read() || things::bottom.read());
 
     short current = things::first.read(0, 7);
 
     short inferior = 0;
     short superior = 7;
 
-    for (auto &part: block) {
-      short edges[2][2] = {
-        {0, 7},
-        {7, 0}
-      };
+    for (auto &part : block) {
+        short edges[2][2] = {{0, 7}, {7, 0}};
 
-      short line = part.get(true);
-      short column = part.get(false);
+        short line = part.get(true);
+        short column = part.get(false);
 
-      for (short inferior = 0; inferior < column; inferior++) {
-        if (frame.get(line, inferior)) {
-          edges[0][0] = inferior + 1;
+        for (short inferior = 0; inferior < column; inferior++) {
+            if (frame.get(line, inferior)) {
+                edges[0][0] = inferior + 1;
+            }
         }
-      }
 
-      for (short superior = 7; superior > column; superior--) {
-        if (frame.get(line, superior)) {
-          edges[0][1] = superior - 1;
+        for (short superior = 7; superior > column; superior--) {
+            if (frame.get(line, superior)) {
+                edges[0][1] = superior - 1;
+            }
         }
-      }
 
-      for (short index = 0; index < 4; index++) {
-        if (line == part.get(true)) {
-          edges[1][0] = min(edges[1][0], part.get(false));
-          edges[1][1] = max(edges[1][1], part.get(false));
+        for (short index = 0; index < 4; index++) {
+            if (line == part.get(true)) {
+                edges[1][0] = min(edges[1][0], part.get(false));
+                edges[1][1] = max(edges[1][1], part.get(false));
+            }
         }
-      }
 
-      inferior = max(inferior, position + edges[0][0] - edges[1][0]);
-      superior = min(superior, position + edges[0][1] - edges[1][1]);
+        inferior = max(inferior, position + edges[0][0] - edges[1][0]);
+        superior = min(superior, position + edges[0][1] - edges[1][1]);
     }
 
     position = current;
-    
+
     if (position < inferior) {
-      position = inferior;
+        position = inferior;
     }
     if (position > superior) {
-      position = superior;
+        position = superior;
     }
 
     rotate[0] = things::second.read(0, 3);
 
     if (rotate[0] == rotate[1]) {
-      rotation = 0;
-    }
-    else {
-      rotation = (rotate[0] < rotate[1] ? 3 : 1);
+        rotation = 0;
+    } else {
+        rotation = (rotate[0] < rotate[1] ? 3 : 1);
     }
 
     rotate[1] = rotate[0];
 
     if (change) {
-      piece = numerics::rand(0, 6);
+        piece = numerics::rand(0, 6);
 
-      generate();
+        generate();
 
-      short shift = 0;
+        short shift = 0;
 
-      for (auto &part: block) {
-        shift = max(shift, part.get(true));
-      }
+        for (auto &part : block) {
+            shift = max(shift, part.get(true));
+        }
 
-      block.translate(-shift - 1, 0);
+        block.translate(-shift - 1, 0);
 
-      change = false;
+        change = false;
     }
 
     steps = 20 - 4 * level;
     count = 1 + count % steps;
-  }
+}
 
-  void draw() {
+void draw() {
     things::frame.set(block, true);
 
     for (short index = 0; index < 8; index++) {
-      things::frame[index] |= frame[index];
+        things::frame[index] |= frame[index];
     }
 
     short points = score;
 
     for (short index = 3; index > -1; index--) {
-      things::layer.set(index, points % 10);
+        things::layer.set(index, points % 10);
 
-      points /= 10;
+        points /= 10;
     }
-  }
+}
 
-  bool collision() {
-    for (auto &part: block) {
-      short line = part.get(true);
-      short column = part.get(false);
+bool collision() {
+    for (auto &part : block) {
+        short line = part.get(true);
+        short column = part.get(false);
 
-      if (line == 7) {
-        return true;
-      }
-      if (frame.get(line + 1, column)) {
-        return true;
-      }
+        if (line == 7) {
+            return true;
+        }
+        if (frame.get(line + 1, column)) {
+            return true;
+        }
     }
 
     return false;
-  }
+}
 
-  void repos() {
+void repos() {
     short shift = position - block[1].get(false);
 
     block.translate(0, shift);
 
     if (count == steps) {
-      if (collision()) {
-        frame.set(block, true);
+        if (collision()) {
+            frame.set(block, true);
 
-        for (short position = 0; position < 8; position++) {
-          if (frame[position] == 255) {
-            for (short index = position; index > 0; index--) {
-              frame[index] = frame[index - 1];
+            for (short position = 0; position < 8; position++) {
+                if (frame[position] == 255) {
+                    for (short index = position; index > 0; index--) {
+                        frame[index] = frame[index - 1];
+                    }
+
+                    score++;
+
+                    frame[0] = 0;
+                }
             }
 
-            score++;
-
-            frame[0] = 0;
-          }
+            change = true;
+            return;
         }
 
-        change = true;
-        return;
-      }
-
-      block.translate(1, 0);
+        block.translate(1, 0);
     }
 
     block = geometry::orthos(block, block[1], rotation);
 
     if (press) {
-      while (not collision()) {
-        block.translate(1, 0);
-      }
+        while (not collision()) {
+            block.translate(1, 0);
+        }
 
-      press = false;
+        press = false;
     }
 
     things::frame.set(block, true);
-  }
+}
 
-  bool verify() {
+bool verify() {
     if (count == steps) {
-      if (collision()) {
-        for (auto &part: block) {
-          short line = part.get(true);
+        if (collision()) {
+            for (auto &part : block) {
+                short line = part.get(true);
 
-          if (line < 0) {
-            return true;
-          }
+                if (line < 0) {
+                    return true;
+                }
+            }
         }
-      }
     }
 
     return false;
-  }
+}
 
-  void update(int value) {
+void update(int value) {
     level = value;
 
     read();
     repos();
     draw();
-  }
 }
+}  // namespace tetris
 
 #endif
